2a9e6f762294813e516d69636980691d0adc9429
Jon Eckerth <48563777+Jo-Eck@users.noreply.github.com>
2023-08-19 20:26:35 -0700

    Heydar20 (#3)
    * implements CICD
    
    ---------
    
    Co-authored-by: Jon Eckerth <eckerth@heydar20.labs.hpecorp.net>

---
+++ project/Kubernetes_Setup/ci_cd/jenkins/custom_jenkins_container/build.sh
@@ -0,0 +1,9 @@
+# This file is to document my bash command to build the container:
+
+docker build -t heydar20.labs.hpecorp.net:31320/custom-jenkins \
+        -f custom_jenkins.dockerfile \
+        --build-arg HTTP_PROXY="http://proxy.its.hpecorp.net:80" \
+        --build-arg NO_PROXY="localhost,cluster.local,.cluster.local,.labs.hpecorp.net,127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,10.244.0.0/16,10.96.0.0/12,10.93.0.0/16,10.152.0.0/16" .
+
+
+
