845d8f3acf025785737bb245fac5e0b0774cafd8
Jon Eckerth <48563777+Jo-Eck@users.noreply.github.com>
2023-08-28 10:12:26 -0700

    adding pachyderm python module

--- project/Kubernetes_Setup/ci_cd/jenkins/custom_jenkins_container/build.sh
+++ project/Kubernetes_Setup/ci_cd/jenkins/custom_jenkins_container/build.sh
@@ -4,6 +4,4 @@ docker build -t heydar20.labs.hpecorp.net:31320/custom-jenkins \
         -f custom_jenkins.dockerfile \
         --build-arg HTTP_PROXY="http://proxy.its.hpecorp.net:80" \
         --build-arg NO_PROXY="localhost,cluster.local,.cluster.local,.labs.hpecorp.net,127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,10.244.0.0/16,10.96.0.0/12,10.93.0.0/16,10.152.0.0/16" .
-
-
-
+docker push heydar20.labs.hpecorp.net:31320/custom-jenkins
